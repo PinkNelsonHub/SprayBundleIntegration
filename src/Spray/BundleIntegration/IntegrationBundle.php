853242afeedd7a3e6373bf58542df46054db77f1
Jur Balledux <jurjean@gmail.com>
2014-03-25 16:18:02 +0100

    Added message you should not use it

--- src/Spray/BundleIntegration/IntegrationBundle.php
+++ src/Spray/BundleIntegration/IntegrationBundle.php
@@ -5,6 +5,10 @@ namespace Spray\BundleIntegration;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
+ * DO NOT USE THIS BUNDLE IN YOUR APPLICATION
+ * 
+ * This bundle is intended to be used for unittests for this project only.
+ * 
  * IntegrationBundle
  */
 class IntegrationBundle extends Bundle
